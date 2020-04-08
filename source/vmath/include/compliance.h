46d6411fe1ff9e4678677178eed1d8e2f64ec90a
Joe Turner <joe@agavemountain.com>
2020-04-07 23:30:22 -0500

    reformatting doxygen

--- source/vmath/include/compliance.h
+++ source/vmath/include/compliance.h
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /**
  *  @file: compliance.h
- *
- *  Compliance calculations
+ *  @brief Compliance calculations
+ *  @author Joe Turner <joe@rxjet.net>
  * 
- *  Copyright (C) 2020  Joe Turner <joe@agavemountain.com>
+ *  Copyright (C) 2020  RxJet Inc.
  */
 #pragma once
 
@@ -15,75 +15,75 @@ namespace math {
 /*! \defgroup compliance Pulmonary compliance
     \brief Pulmonary compliance (Lung compliance) and Resistance
 
-Lung compliance, is a measure of the lung's elasisticy. In simple terms, pulmonary 
-compliance is calculated using the following equation, where ΔV is the 
-change in volume, and ΔP is the change in pleural pressure:
-    \f[
-       compliance = \frac{\Delta Volume}{\Delta Pressure}\\
-    \f]
+    Lung compliance, is a measure of the lung's elasisticy. In simple terms, pulmonary 
+    compliance is calculated using the following equation, where ΔV is the 
+    change in volume, and ΔP is the change in pleural pressure:
+        \f[
+        compliance = \frac{\Delta Volume}{\Delta Pressure}\\
+        \f]
 
-- Low compliance indicates a stiff lung (one with high elastic recoil) 
-and can be thought of as a thick balloon – this is the case often seen 
-in fibrosis. 
+    - Low compliance indicates a stiff lung (one with high elastic recoil) 
+    and can be thought of as a thick balloon – this is the case often seen 
+    in fibrosis. 
 
-- Lungs with low compliance are stiff lungs and will require much 
-greater pressure to reach a given volume compared to lungs that 
-have high compliance.
+    - Lungs with low compliance are stiff lungs and will require much 
+    greater pressure to reach a given volume compared to lungs that 
+    have high compliance.
 
-- High compliance indicates a pliable lung (one with low elastic recoil) 
-and can be thought of as a grocery bag – this is the case often seen 
-in emphysema. 
+    - High compliance indicates a pliable lung (one with low elastic recoil) 
+    and can be thought of as a grocery bag – this is the case often seen 
+    in emphysema. 
 
-- Compliance is highest at moderate lung volumes, and much lower at volumes 
-which are very low or very high. The compliance of the lungs 
-demonstrate lung hysteresis; that is, the compliance is different 
-on inspiration and expiration for identical volumes.
+    - Compliance is highest at moderate lung volumes, and much lower at volumes 
+    which are very low or very high. The compliance of the lungs 
+    demonstrate lung hysteresis; that is, the compliance is different 
+    on inspiration and expiration for identical volumes.
 
-- Normal chest wall compliance in adults is approximately 100-200ml/cm H2O. 
+    - Normal chest wall compliance in adults is approximately 100-200ml/cm H2O. 
 
-- Children have far lower chest wall compliance at 2.5-5.0ml/cm H2O.
+    - Children have far lower chest wall compliance at 2.5-5.0ml/cm H2O.
 
-### Static Compliance
+    ### Static Compliance
 
-Static compliance (\f$ C_{stat} \f$) is defined as the change in lung volume per unit change in 
-pressure in the absence of flow. Because it represents compliance during 
-periods without gas flow, it is usually measured during an inspiratory 
-pause or inspiratory hold maneuver.
+    Static compliance (\f$ C_{stat} \f$) is defined as the change in lung volume per unit change in 
+    pressure in the absence of flow. Because it represents compliance during 
+    periods without gas flow, it is usually measured during an inspiratory 
+    pause or inspiratory hold maneuver.
 
-The equation is:
-    \f[
-       C_{stat} = \frac{V_T}{(P_{plat} - PEEP)}\\
-    \f]
+    The equation is:
+        \f[
+        C_{stat} = \frac{V_T}{(P_{plat} - PEEP)}\\
+        \f]
 
-### Dynamic Compliance
+    ### Dynamic Compliance
 
-Dynamic compliance (\f$ C_{dyn} \f$) is defined as the change in lung volume per unit change
-in pressure at any given time during actual movement of air.
+    Dynamic compliance (\f$ C_{dyn} \f$) is defined as the change in lung volume per unit change
+    in pressure at any given time during actual movement of air.
 
-The equation is:
-    \f[
-    C_{dyn} = \frac{V_T}{(PIP - PEEP)}\\
-    \f]
- 
-## Elastance
+    The equation is:
+        \f[
+        C_{dyn} = \frac{V_T}{(PIP - PEEP)}\\
+        \f]
+    
+    ## Elastance
 
-Elasticity is the tendency of a material to try to maintain its
-shape and offer resistance to stretching forces. When interpreted according to Hooke’s
-Law, elastance is defined in the following equation: 
-    \f[
-       E = \frac{\Delta P}{\Delta V}\\
-    \f]
+    Elasticity is the tendency of a material to try to maintain its
+    shape and offer resistance to stretching forces. When interpreted according to Hooke’s
+    Law, elastance is defined in the following equation: 
+        \f[
+        E = \frac{\Delta P}{\Delta V}\\
+        \f]
 
-Where ∆P is the change in pressure applied to the lung and ∆V is the change in volume in the lung
+    Where ∆P is the change in pressure applied to the lung and ∆V is the change in volume in the lung
 
-## Resistance
-Frictional resistance occurs when a air flows through the respiratory system. The resistance (R) 
-of the tube corresponds to the ratio between the pressure difference (∆P) between the 
-two extremities of the tube, and the flow rate (V') of the fluid :
+    ## Resistance
+    Frictional resistance occurs when a air flows through the respiratory system. The resistance (R) 
+    of the tube corresponds to the ratio between the pressure difference (∆P) between the 
+    two extremities of the tube, and the flow rate (V') of the fluid :
 
-    \f[
-       R = \frac{\Delta P}{V'}\\
-    \f]
+        \f[
+        R = \frac{\Delta P}{V'}\\
+        \f]
 
 */
 
@@ -92,7 +92,9 @@ two extremities of the tube, and the flow rate (V') of the fluid :
      * 
      * This function will calculate dynamic compliance.
      * 
-     * Dynamic compliance is defined as the change in lung volume per unit change in pressure in the presence of flow. Its components are
+     * Dynamic compliance is defined as the change in lung volume per unit change in 
+     * pressure in the presence of flow. Its components are:
+     * 
      * - Chest wall compliance
      * - Lung tissue compliance
      * - Airway resistance (which makes it frequency-dependent)
@@ -119,7 +121,9 @@ two extremities of the tube, and the flow rate (V') of the fluid :
      * 
      * This function will calculate static compliance.
      * 
-     * Static compliance is defined as the change in lung volume per unit change in pressure in the absence of flow. 
+     * Static compliance is defined as the change in lung volume per unit 
+     * change in pressure in the absence of flow. 
+     * 
      * It is composed of:
      * 
      * - Chest wall compliance (usually 200ml/cm H2O.
@@ -142,5 +146,22 @@ two extremities of the tube, and the flow rate (V') of the fluid :
         return (tidal_vol_ml / (Pplat - PEEP));
     }
 
-}
-}
+    /**
+     * \brief Calculate Airway Resistance (Raw)
+     * 
+     * In the ventilated patient, resistance can be measured by dividing the 
+     * [peak pressure minus the plateau pressure] by the flowrate in litres per second.
+     * 
+     * The equation is:
+     * \f[
+     *   Raw = \frac{(PIP - PEEP)}{Lps}\\
+     * \f]
+     * 
+     * 
+     */
+    inline double calculate_Raw(double Pplat, double Ppeak, double liters_per_second)
+    {
+        return ( (Ppeak - Pplat ) / liters_per_second );
+    }
+} // math
+} // ovp
